f0a6cd8a07524b08c5662d76256e67e8de22b76c
Wajeeh Ul Hassan <wajeehulhassan_vii@hotmail.com>
2020-04-30 02:32:50 +1000

    some changes for production environment

--- flask/app.py
+++ flask/app.py
@@ -27,5 +27,5 @@ import views   # noqa: E402
 # import myadmin
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=8000, debug=True)
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
+    # app.run(host='127.0.0.1', port=8000, debug=True)
